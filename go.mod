dfb3e21ec735ef22546decf3b6df04819cfd2707
Sonya <sonyacore@protonmail.com>
2023-01-09 02:13:43 +0330

    Added Banner

--- go.mod
+++ go.mod
@@ -6,11 +6,11 @@ require github.com/shirou/gopsutil/v3 v3.22.12
 
 require (
 	github.com/go-ole/go-ole v1.2.6 // indirect
-	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
-	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
-	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
+	github.com/lufia/plan9stats v0.0.0-20220913051719-115f729f3c8c // indirect
+	github.com/power-devops/perfstat v0.0.0-20221212215047-62379fc7944b // indirect
+	github.com/shirou/gopsutil v3.21.11+incompatible // direct
 	github.com/tklauser/go-sysconf v0.3.11 // indirect
 	github.com/tklauser/numcpus v0.6.0 // indirect
 	github.com/yusufpapurcu/wmi v1.2.2 // indirect
-	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"proc-top/colors"
 	cpu "proc-top/src/cpu"
 	disk "proc-top/src/disk"
 	"proc-top/src/host"
@@ -15,10 +16,35 @@ import (
 
 var (
 	name     = "ProcTop"
-	version  = "0.4.0"
+	version  = "0.5.0"
 	build    = "Custom"
 	codename = "ProcTop , CLI monitor tool."
 )
+
+var (
+	Purple = colors.Purple
+	res    = colors.Reset
+)
+
+const (
+	banner = `     
+============================================
+| ______                 _____             |
+| | ___ \               |_   _|            |
+| | |_/ / __ ___   ___    | | ___  _ __    |
+| |  __/ '__/ _ \ / __|   | |/ _ \| '_ \   |
+| | |  | | | (_) | (__    | | (_) | |_) |  |
+| \_|  |_|  \___/ \___|   \_/\___/| .__/   |
+|                                 | |      |
+|                                 |_|      |
+============================================
+`
+)
+
+func Header() {
+	fmt.Println(VersionStatement())
+	fmt.Println(Purple, banner, res)
+}
 
 func Version() string {
 	return version
@@ -29,10 +55,6 @@ func VersionStatement() string {
 	return strings.Join([]string{
 		"ProcTop ", Version(), " (", codename, ") ", build, " (", runtime.Version(), " ", runtime.GOOS, "/", runtime.GOARCH, ")",
 	}, "")
-}
-
-func banner() string {
-	return "PROC TOP"
 }
 
 func main() {
@@ -50,12 +72,12 @@ func main() {
 
 	flag.Parse()
 	for {
+		utils.CallClear()
+		Header()
 		if *versionflag {
 			fmt.Println(name, version)
 			break
 		}
-		utils.CallClear()
-		fmt.Println(VersionStatement())
 		if *full {
 			host.KernelInfo()
 			memory.Memory()
